1f0ce3f1f6c86615fcaa8479a9229d65f5f856da
Zack Fertig <61804342+zfert514@users.noreply.github.com>
2021-11-02 14:03:51 -0400

    added pickups

---
+++ Ball Rolling/Assets/Scripts/GolfBallController.cs
@@ -0,0 +1,157 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GolfBallController : MonoBehaviour
+{
+    private Rigidbody _rb;
+    public float speed;
+    private int _count;
+    private int _points;
+    public Text countText;
+    public Text pointsText;
+    public Text winText;
+    public GameObject startText;
+    public Text timerText;
+    public GameObject restartText;
+    public Slider slider;
+    public float jumpForce;
+
+    private bool grounded;
+    private Vector3 _reset;
+    private float _startTime;
+    private int _num;
+    private float _time;
+    private float _power;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _count = 0;
+        _points = 0;
+        SetCountText();
+        winText.text = "";
+        timerText.text = "";
+        _reset = transform.position;
+        restartText.SetActive(false);
+        _power = 1f;
+        grounded = true;
+
+        _num = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //num is so thet this only happens once and it only starts counting after this is done
+        if(transform.position != _reset && _num == 0)
+        {
+            startText.SetActive(false);
+            _startTime = Time.time;
+
+            _num++;
+        }
+        if(_num > 0 && winText.text == "")
+        {
+            _time = Time.time - _startTime;
+
+            string seconds = _time.ToString("f2");
+
+            timerText.text = seconds + "s";
+            
+            if(_time >= 120)
+            {
+                winText.text = "Level Failed!";
+            }
+        }
+        if(winText.text != "")
+        {
+            restartText.SetActive(true);
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+                string currentSceneName = SceneManager.GetActiveScene().name;
+                SceneManager.LoadScene(currentSceneName);
+            }
+        }
+        slider.value += _power * Time.deltaTime;
+        if (slider.value == slider.maxValue || slider.value == slider.minValue)
+        {
+            _power = -_power;
+        }
+        if (Input.GetMouseButtonDown(0) && grounded)
+        {
+            _rb.AddRelativeForce(Vector3.forward * slider.value * speed, ForceMode.Impulse);
+            _count++;
+            SetCountText();
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //float moveHorizontal = Input.GetAxis("Horizontal");
+        //float moveVertical = Input.GetAxis("Vertical");
+
+        //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            _points++;
+            SetCountText();
+        }
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            winText.text = "Level Failed!";
+            restartText.SetActive(true);
+            gameObject.SetActive(false);
+        }
+        if (other.gameObject.CompareTag("deathPlane") && !winText.text.Equals("You Win!"))
+        {
+            winText.text = "Level Failed!";
+            restartText.SetActive(true);
+            gameObject.SetActive(false);
+        }
+        if (other.gameObject.CompareTag("Finish"))
+        {
+            winText.text = "You Win!";
+            restartText.SetActive(true);
+        }
+    }
+
+    void OnCollisionStay(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.CompareTag("Ground"))
+        {
+            grounded = true;
+        }
+        if (collisionInfo.collider.CompareTag("Finish"))
+        {
+            winText.text = "You Win!";
+        }
+    }
+    void OnCollisionExit(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.CompareTag("Ground"))
+        {
+            grounded = false;
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Stroke: " + _count.ToString();
+        pointsText.text = "Points: " + _points.ToString();
+    }
+}
---
+++ Ball Rolling/Assets/Scripts/GolfCameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GolfCameraController : MonoBehaviour
+{
+    public GameObject player;
+    public Text winText;
+
+    private Vector3 _offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (winText.text != "" && Input.GetKeyDown(KeyCode.R))
+        {
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentSceneName);
+        }
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + _offset;
+    }
+}
